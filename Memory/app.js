abd87a3eadfbd892d6e9f388f2df27e960f3fe41
Filipcio12 <121309536+Filipcio12@users.noreply.github.com>
2023-04-22 23:55:06 +0200

    Add files via upload

--- Memory/app.js
+++ Memory/app.js
@@ -1,31 +1,44 @@
-let canvas = document.getElementById("canvas")
-let ctx = canvas.getContext("2d")
-ctx.moveTo(0, 0)
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+ctx.moveTo(0, 0);
 
-WIDTH = canvas.width
-HEIGHT = canvas.height
+WIDTH = canvas.width;
+HEIGHT = canvas.height;
 
 class Card {
     constructor() {
-        this.side = "img/back"
-        this.front
-        this.back
-        this.x
-        this.y
-        this.val
+        this.side = back;
+        this.front;
+        this.back = "backR.png";
+        this.x;
+        this.y;
+        this.val;
     }
 }
 
 function loadImage(path) {
-    let image = new Image()
-    image.src = path
-    image.onload
-    return image
+    let image = new Image();
+    image.src = path;
+    image.onload;
+    return image;
 }
 
 function main() {
-    // Creating cards
-    ctx.drawImage(loadImage("img/kh.png"), 10, 10)
+    let paths = [];
+
+    for (let i = 1; i < 14; i++) {
+        paths.push("Cards/" + i + "C.png");
+        paths.push("Cards/" + i + "D.png");
+        paths.push("Cards/" + i + "H.png");
+        paths.push("Cards/" + i + "S.png");
+    }
+
+    let n = Math.floor(Math.random() * 51);
+    image = new Image();
+    image.src = paths[n];
+    image.onload = () => {
+        ctx.drawImage(image, 10, 10);
+    }
 }
 
 main()
